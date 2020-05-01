c1eeee3ff5ded2a15f05538bee5e4c2e76521dbc
thili <thili@Laptop-0J187AN>
2020-05-02 01:58:14 +0530

    First Commit

---
+++ app/src/app/app.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.io.File;
+import java.io.IOException;
+
+public class app {
+
+	public static void main(String[] args) throws IOException {
+		
+		File file = new File("D:\\pictures\\NewTextFile.txt");
+		file.createNewFile();
+		System.out.println("New File Created");
+	
+		
+		
+		
+	}
+	
+
+
+}
+		
+	
